e2ceeea6ea02b9949a6178219c177c14bbba3622
iliziumlion <iliziumlion@gmail.com>
2016-08-19 15:26:15 +0700

    Merge branch 'ilizium'
    # Conflicts:
    #	config.codekit
    #	css/main.css
    #	js/main.js
    #	sass/main.scss

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
   $('.partner').slick({
     dots: true,
 	  infinite: true,
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+	  arrows: false,
+	  responsive:true,
+	  centerPadding: '60px',
+=======
+>>>>>>> origin/master
+>>>>>>> ilizium
 	  speed: 300,
 	  slidesToShow: 4,
 	  slidesToScroll: 4,
@@ -34,4 +43,69 @@ $(document).ready(function(){
 	    // instead of a settings object
 	  ]
   });
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+});
+
+$(document).ready(function(){
+  $('.sliderFon').slick({
+    dots: true,
+	  infinite: false,
+	  arrows: false,
+	  autoplay: true,
+  	autoplaySpeed: 5000,
+  	speed: 500,
+	  fade: true,
+	  cssEase: 'linear',
+	  adaptiveHeight: true,
+	  responsive: [
+	  	{
+	      breakpoint: 1920,
+	      settings: {
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 1366,
+	      settings: {
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 1200,
+	      settings: {
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+  });
+=======
+>>>>>>> origin/master
+>>>>>>> ilizium
 });
